e1f4aa42df7e13c7a72d21320525e0d42ea77eff
Darryl Orleans <darryl.orleans@binus.ac.id>
2024-10-16 16:39:25 +0700

    NPC System Created

--- Assets/Scripts/NPC/NPC_Child.cs
+++ Assets/Scripts/NPC/NPC_Child.cs
@@ -1,8 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class NPC_Child : MonoBehaviour
 {
-  
+    public Transform Destination;
+    public NPC_Main npcMain;
+    public GameObject NPC_Dialogue;
+    private bool isWalking = true;
+    private bool hasInteracted = false;
+
+    public float speed = 3f;
+    public float dialogueDisplayTime = 2f;
+
+    void Update()
+    {
+        if (isWalking)
+        {
+            WalkTowardsDestination();
+        }
+    }
+
+    public void Walk()
+    {
+        isWalking = true;
+    }
+
+    private void WalkTowardsDestination()
+    {
+        if (Destination != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Destination.position, speed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, Destination.position) < 0.1f)
+            {
+                isWalking = false;
+                npcMain.Walking = false;
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (!hasInteracted)
+        {
+            StopNPCAndShowDialogue();
+        }
+    }
+
+    private void StopNPCAndShowDialogue()
+    {
+        isWalking = false;
+        NPC_Dialogue.SetActive(true);
+        StartCoroutine(ResumeWalkingAfterDialogue());
+        hasInteracted = true;
+    }
+
+    private IEnumerator ResumeWalkingAfterDialogue()
+    {
+        yield return new WaitForSeconds(dialogueDisplayTime);
+        NPC_Dialogue.SetActive(false);
+        isWalking = true;
+    }
 }
--- Assets/Scripts/NPC/NPC_Main.cs
+++ Assets/Scripts/NPC/NPC_Main.cs
@@ -1,8 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class NPC_Main : MonoBehaviour
 {
-    
+    public static NPC_Main Instance { get; private set; }
+
+    public GameObject[] NPCs;
+    public bool Walking = false;
+    public float delayBetweenNPCs = 2f;
+
+    private int currentNPCIndex = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        foreach (GameObject npc in NPCs)
+        {
+            npc.SetActive(false);
+        }
+    }
+
+    public void StartSystem()
+    {
+        StartCoroutine(StartTheSystem());
+    }
+
+    private void ActivateNextNPC()
+    {
+        if (currentNPCIndex < NPCs.Length)
+        {
+            NPCs[currentNPCIndex].SetActive(true);
+            NPC_Child npcChild = NPCs[currentNPCIndex].GetComponent<NPC_Child>();
+            npcChild.Walk();
+           
+        }
+    }
+
+    public IEnumerator WaitAndActivateNextNPC()
+    {
+        if (currentNPCIndex < NPCs.Length)
+        {
+            Walking = true;
+            yield return new WaitForSeconds(delayBetweenNPCs);
+            currentNPCIndex++;
+            ActivateNextNPC();
+        }
+    }
+
+    private void Update()
+    {
+        if (!Walking)
+        {
+            StartCoroutine(WaitAndActivateNextNPC());
+        }
+    }
+
+    private IEnumerator StartTheSystem()
+    {
+        yield return new WaitForSeconds(delayBetweenNPCs);
+        
+        if (NPCs.Length > 0)
+        {
+            ActivateNextNPC();
+        }
+    }
 }
